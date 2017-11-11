a6e07822d3203513bb8984ed11e807fc44cd02dd
kumavis <aaron@kumavis.me>
2017-11-10 17:58:22 -0800

    load assets from leveldb memdown

--- bootloader.js
+++ bootloader.js
@@ -1,4 +1,12 @@
+const pify = require('pify')
 const SwGlobalListener = require('sw-stream/lib/sw-global-listener.js')
+const createDb = require('./db')
+
+
+//
+// establish connection
+//
+
 const connectionListener = new SwGlobalListener(global)
 global.addEventListener('message', console.log)
 
@@ -6,4 +14,34 @@ console.log('bootloader online')
 
 connectionListener.on('remote', (portStream, messageEvent) => {
   console.log('page connection established')
+})
+
+
+//
+// connect to fs
+//
+
+const db = createDb()
+
+// fixture data
+db.put('/', Buffer.from('welcome home'), console.log)
+db.put('/xyz', Buffer.from('i n   y o u r   b r o w s e r'), console.log)
+
+
+global.addEventListener('fetch', (event) => {
+  const req = event.request
+  if (req.method !== 'GET') {
+    console.log('not found')
+    event.respondWith(new Response('not found',{ status: 404 }))
+  }
+
+  event.respondWith((async () => {
+    const url = new URL(req.url)
+    const path = url.pathname
+    // const body = `will return data for "${path}"`
+    // const body = await pify(fs.readFile.bind(fs))(path)
+    const body = await db.get(path)
+    console.log(body)
+    return new Response(body)
+  })())
 })
---
+++ db.js
@@ -0,0 +1,12 @@
+// const leveljs = require('level-js')
+const memdown = require('memdown')
+const levelup = require('levelup')
+
+module.exports = createFs
+
+function createFs() {
+  // const db = new levelup(leveljs('root-disk'))
+  const db = new levelup(new memdown())
+
+  return db
+}
